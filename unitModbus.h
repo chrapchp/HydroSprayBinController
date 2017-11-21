a40298e21d5a4164e77df2c4ceeb2da4704d60c0
unknown <chrapchp@flashsense.com>
2017-11-21 08:45:22 -0800

    commissioned

--- unitModbus.h
+++ unitModbus.h
@@ -41,7 +41,7 @@
 uint16_t modbusRegisters[MODBUS_REG_COUNT];
 
 #define MB_SPEED 			    19200
-#define MB_SLAVE_ID				11
+#define MB_SLAVE_ID				14
 #define MB_SERIAL_PORT			0
 #define MB_MAX485_PIN			6  // set to zero for RS-232
 
